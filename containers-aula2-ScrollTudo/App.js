f64026614b082a92c3412a35d52c6c22603b9560
pdr-tuche <contato.nevespedro@gmail.com>
2022-04-03 00:48:56 -0300

    primeiro comit aulas

---
+++ containers-aula2-ScrollTudo/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Text, ScrollView, View, Image } from 'react-native';
+
+import Capa from './assets/capa.png';
+import estilos from './estilos';
+
+export default function App() {
+  return (
+    //foi adicionado o scroll sobre todo o conteudo
+    <ScrollView style={estilos.container}> 
+      <Image source={ Capa } style={ estilos.imagem }/>
+
+      <Text style={estilos.titulo}>The Beatles</Text>
+      <Text style={estilos.subtitulo}>Help</Text>
+
+      <View>
+        <Text style={estilos.paragrafo}>
+          Help, I need somebody
+          Help, not just anybody
+          Help, you know I need someone, help!
+        </Text>
+
+        <Text style={estilos.paragrafo}>
+          When I was younger, so much younger than today
+          I never needed anybody's help in any way
+          But now these days are gone, I'm not so self assured
+          Now I find I've changed my mind and opened up the doors
+        </Text>
+
+        <Text style={estilos.paragrafo}>
+          Help me if you can, I'm feeling down
+          And I do appreciate you being round
+          Help me, get my feet back on the ground
+          Won't you please, please help me?
+        </Text>
+
+        <Text style={estilos.paragrafo}>
+          And now my life has changed in oh so many ways
+          My independence seems to vanish in the haze
+          But every now and then I feel so insecure
+          I know that I just need you like I've never done before
+        </Text>
+
+        <Text style={estilos.paragrafo}>
+          Help me if you can, I'm feeling down
+          And I do appreciate you being round
+          Help me, get my feet back on the ground
+          Won't you please, please help me?
+        </Text>
+
+        <Text style={estilos.paragrafo}>
+          When I was younger, so much younger than today
+          I never needed anybody's help in any way
+          But now these days are gone, I'm not so self assured
+          Now I find I've changed my mind and opened up the doors
+        </Text>
+
+        <Text style={estilos.paragrafo}>
+          Help me if you can, I'm feeling down
+          And I do appreciate you being round
+          Help me, get my feet back on the ground
+          Won't you please, please help me?
+        </Text>
+      </View>
+    </ScrollView>
+  );
+}
---
+++ containers-aula2-ScrollTudo/estilos.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+
+const estilos = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginTop: 30,
+    flex: 1
+  },
+  imagem: {
+    width: 300,
+    height: 300,
+    marginBottom: 20
+  },
+  titulo: {
+    fontSize: 26,
+    fontWeight: 'bold',
+  },
+  subtitulo: {
+    fontSize: 22,
+    marginBottom: 20
+  },
+  paragrafo: {
+    marginBottom: 20,
+    fontSize: 18,
+  },
+});
+
+export default estilos;
